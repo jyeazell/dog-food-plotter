a0a2b92769c9ad2dd3547d2a55bde2d228e4627e
Jeff Yeazell <jyeazell@mac.com>
2021-08-27 14:48:10 -0700

    building table

--- sandbox.R
+++ sandbox.R
@@ -9,4 +9,24 @@ page_info <- read_html(my_url)
 
 feeding_amounts <- page_info %>% 
   html_element("table") %>% 
-  html_table()
+  html_table(na.strings = "")
+
+names(feeding_amounts) <- c("dog_wt",
+                           "age_00_04",
+                           "age_04_09",
+                           "age_10_12",
+                           "age_13_18")
+
+feeding_amounts <- feeding_amounts %>% 
+  pivot_longer(cols = starts_with("age"),
+               names_to = "puppy_age",
+               values_to = "feed_amt") %>% 
+  drop_na()
+
+feeding_amounts_t <- feeding_amounts %>% 
+  separate(col = dog_wt,
+           into = c("dog_wt_lb",
+                    "dog_wt_kg"),
+           sep = " ",
+           remove = FALSE)
+  
